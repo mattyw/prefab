49caf6523ced2e468cffcd915f2805026dcb006e
Sam Carter <sam.w.carter@gmail.com>
2013-09-28 15:35:05 +0100

    Added ring to the dependencies

--- project.clj
+++ project.clj
@@ -16,6 +16,8 @@
                  [clj-rome "0.4.0"]
                  [clj-rome-fetcher "0.1.0"]
                  ]
+  :plugins [[lein-ring "0.8.7"]]
+                 
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]]
                    :source-paths ["dev"]
                    :plugins [[lein-midje "3.1.2"]]}})
