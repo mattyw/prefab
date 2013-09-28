b403a97c89f60a28b685c840302d45bf5fc0a918
loganlinn <logan.linn@gmail.com>
2013-09-27 19:53:46 -0700

    Basic routes

--- src/prefab/routes.clj
+++ src/prefab/routes.clj
@@ -1,10 +1,20 @@
 (ns prefab.routes
   (:require [compojure.core :refer :all]
-            [compojure.handler]))
+            [compojure.handler]
+            [compojure.route :as route]
+            [ring.util.response :as resp :refer (response)]
+            ))
 
 
 (defn app [system]
   (->
     (routes
-      (GET "/" [] "Hello, prefab-world!"))
+      (GET "/feed/:id" [id]
+           (response "TODO fetch & show feed"))
+      (POST "/feed" {params :params}
+            (response "TODO create feed"))
+
+      (GET "/" [] "Hello, prefab-world!")
+      (route/resources "/")
+      (route/not-found "Not found."))
     compojure.handler/site))
