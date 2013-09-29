1bb3805677e156f8cdaa3673862f22dfe50ce4c3
loganlinn <logan.linn@gmail.com>
2013-09-29 13:55:33 -0700

    Add quick prefetch script

---
+++ src/prefab/prepopulate.clj
@@ -0,0 +1,29 @@
+(ns prefab.prepopulate
+  (:require [org.httpkit.client :as http]
+            [environ.core :refer (env)]
+            [taoensso.timbre :refer (info infof error)]))
+
+(def feeds
+  [{:name "ClojureCup Blog"
+    :urls ["http://clojurecup.com/blog/feed.xml"]}
+   {:name "Reddit Frontpage"
+    :urls ["http://www.reddit.com/r/all.rss"]}
+   {:name "Slashdot"
+    :urls ["http://prefab.clojurecup.com/feeds/427546384"]}
+   {:name "NPR News"
+    :urls ["http://www.npr.org/rss/rss.php?id=1001"]}
+   {:name "ESPN News"
+    :urls ["http://sports.espn.go.com/espn/rss/news"]}])
+
+(defn create-feed [host {:keys [name urls] :as feed}]
+  (infof "Creating feed '%s' %s" name urls)
+  (let [options {:form-params {:name name "urls[]" urls} :keepalive 3000}
+        resp @(http/post (str host "/feeds") options)]
+    (when-let [e (:error resp)] (error e))))
+
+(defn -main [& args]
+  (let [host (env :host "http://localhost:8080")
+        create-feed (partial create-feed host)]
+    (info "Starting Prefab Prepopulation")
+    (doseq [feed feeds] (create-feed feed))
+    (info "Done!")))
